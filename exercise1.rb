dc798fee2021ef7688a42410d4ddc8f20d39ab97
Shoshana01 <shanabjacobs@gmail.com>
2020-12-06 22:25:06 -0500

    saving exercise2.rb

--- exercise1.rb
+++ exercise1.rb
@@ -1,6 +1,13 @@
-puts 2
-puts 3
-puts 2 + 3
-puts 2 
-puts 3 
-puts 2 + 3
+# puts 2
+# puts 3
+# puts 2 + 3
+# puts 2 
+# puts 3 
+# puts 2 + 3
+# puts "Ada lived for #{1852-1815} years."
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
+puts "5+4"
+puts "5*3"
---
+++ exercise2.rb
@@ -0,0 +1,36 @@
+# meal_cost=55
+# tip=5
+# meal_cost+=tip
+# puts "The total meal cost is " + meal_cost.to_s
+# #calculating the meal cost + the tip
+# puts "the result is #{45628 * 7839}"
+# #string interpolation
+
+# name= "Sandra"
+# greeting= "Hello #{name}! It's good to see you again."
+# mission= "Your mission, should you choose to accept it..." 
+# puts "#{greeting} #{mission}"
+# #variables
+
+# my_number = 12
+# puts my_number > 10
+# puts my_number < 10
+# your_number = 1
+# puts my_number == your_number
+# puts my_number != your_number
+
+# animals= "cats"
+# number = 20
+# location = "the yard"
+# puts "There are #{number} #{animals} in #{location}!"
+
+# who= "Mrs. Peacock"
+# where= "the library"
+# what = "rope"
+# puts accusation = "It was #{who} in #{where} with the #{what}."
+
+counter = 25
+counter + 1
+# counter = counter+1
+counter +=1
+puts counter
