ebaa59cf30f010decf6bffcfe413541b37c9cd2a
Shoshana01 <shanabjacobs@gmail.com>
2020-12-07 23:32:39 -0500

    saving new changes

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,26 @@
-puts "What is your name?"
-user_name=gets
-puts "Hello, #{user_name}"
+# puts "What is your name?"
+# user_name=gets
+# puts "Hello, #{user_name}"
+# puts "How old are you?"
+# user_age=gets.chomp.to_i
+# # puts "You are #{user_age} years old."
+# puts "You were born in #{2020-user_age}"
+
+# number= gets.to_i
+# if number > 0
+# puts "#{number} is positive"
+# else
+#     puts"#{number} is negative"
+# end
+
+x= gets.to_i
+y= gets.to_i
+if x > y
+    puts "x is greater than y!"
+elsif x < y
+    puts "x is less than y!"
+else
+    puts "x equals y!"
+end
+
+
