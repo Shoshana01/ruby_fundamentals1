6dbc16c0d7b245ccc61ab3b16cc973feaba353c6
Shoshana01 <shanabjacobs@gmail.com>
2020-12-08 18:59:39 -0500

    savingexercise4d

---
+++ exercise4d.rb
@@ -0,0 +1,10 @@
+puts "What's your name?"
+user_name=gets.chomp
+if user_name.length > 10 letters
+    puts "hi, #{user_name}."
+elsif user_name.length < 10 letters
+    puts "hello, #{user_name}."
+elsif user_name.length == 10 letters
+    puts "hey, #{user_name}."
+else 
+end
