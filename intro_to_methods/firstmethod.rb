b52e11d2160176efee1947c4a6f8d6983a586f49
Shoshana01 <shanabjacobs@gmail.com>
2020-12-16 19:12:16 -0500

    learningruby

---
+++ intro_to_methods/firstmethod.rb
@@ -0,0 +1,35 @@
+# def hi
+#     puts "Hi"
+# end
+# hi
+
+# def hi(user_name)
+#     puts "Hello #{user_name}"
+# end
+
+# hi('shana')
+# hi('aleeza')
+
+# def give_me_the_total(first,second)
+#     sum= first + second
+#     puts "Thanks for using this method"
+#     return sum
+# end
+
+# number_of_cats=35
+# number_of_dogs=25
+# total_number_of_pets= give_me_the_total(number_of_cats, number_of_dogs)
+# puts total_number_of_pets
+
+# number_of_movies=120
+# number_of_series=230
+# total_number_of_media= give_me_the_total(number_of_movies, number_of_series)
+
+# def say_hello(name, city)
+#     puts("Hello, #{name} from #{city} ! ")
+# end
+# say_hello("Emma", "Toronto")
+# say_hello("Jackie", "Toronto")
+# say_hello("Brian", "Toronto")
+
+
